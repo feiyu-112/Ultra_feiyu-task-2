357632facf9cc0dbf21febd85bd72442a7ec6fb0
feiyu-112 <feiyu11_2025@qq.com>
2025-11-01 17:37:38 +0800

    Create 5_main.c

---
+++ 5_main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int sum = 0;
+    int i = 0;
+
+    printf("请输入4个整数 （以空格分隔）：");
+    while (i < 4) {
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+        i++;
+    }
+
+    arr[4] = sum;
+
+    i = 0;
+    while (i < 5){
+        if (i == 4) {
+            printf ("%d",arr[i]);
+        }else{
+            printf("%d", arr[i]);
+        }
+        i++;
+    }
+    return 0;
+}
