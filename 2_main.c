53be06d2a29566b798b3aede83f312ec54af8c6e
feiyu-112 <feiyu11_2025@qq.com>
2025-11-01 17:35:47 +0800

    Create 2_main.c

---
+++ 2_main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int num = 100; num <= 999; num++) {
+
+        int hundreds = num / 100;
+        int tens = (num / 10) % 10;
+        int units = num % 10;
+
+        if (hundreds * hundreds * hundreds + 
+        tens * tens * tens +
+        units * units * units == num) {
+            if (num == 153) {
+                printf("%d",num);
+            } else {
+                printf("%d",num);
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
